aaed21a50b1310eb024528059bdc9b1babe12dc3
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-03-09 22:59:09 +0530

    add theme toggle

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -7,10 +7,13 @@ class ThemeContextProvider extends Component {
     isLightTheme : true,
     light : { text: '#555', ui: '#ddd', bg: '#eee' },
     dark: { text: '#ddd', ui: '#333', bg:'#555' }
-  } 
+  }
+  toggleTheme = () => {
+    this.setState({ isLightTheme: !this.state.isLightTheme });
+  }
   render() { 
     return (
-      <ThemeContext.Provider value={{...this.state}}>
+      <ThemeContext.Provider value={{...this.state, toggleTheme: this.toggleTheme}}>
         {this.props.children}
       </ThemeContext.Provider>
     );
