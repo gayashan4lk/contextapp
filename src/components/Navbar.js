daf69874c21b418bcef3d4ea4379b60b170bdf26
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-03-09 23:43:12 +0530

    refractor Navbar Component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,19 +4,21 @@ import { ThemeContext } from '../contexts/ThemeContext';
 class Navbar extends Component {
   static contextType = ThemeContext;
   render() { 
-    console.log(this.context);
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-    console.log(theme);
     return (
-      <nav style={{background: theme.ui, color: theme.text}}>
-        <h1>Context App</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      <ThemeContext.Consumer>{(context) => {
+        const { isLightTheme, light, dark } = this.context;
+        const theme = isLightTheme ? light : dark;
+          return(
+            <nav style={{background: theme.ui, color: theme.text}}>
+                <h1>Context App</h1>
+                <ul>
+                  <li>Home</li>
+                  <li>About</li>
+                  <li>Contact</li>
+                </ul>
+              </nav>
+          )
+      }}</ThemeContext.Consumer>
     );
   }
 }
