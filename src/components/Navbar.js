12d8e20c47bd4124884585e118718f2080cae05a
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-03-10 09:15:36 +0530

    add authContext to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import { AuthContext } from '../contexts/AuthContext';
 
 class Navbar extends Component {
-  static contextType = ThemeContext;
   render() { 
     return (
-      <ThemeContext.Consumer>{(context) => {
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
-          return(
-            <nav style={{background: theme.ui, color: theme.text}}>
-                <h1>Context App</h1>
-                <ul>
-                  <li>Home</li>
-                  <li>About</li>
-                  <li>Contact</li>
-                </ul>
-              </nav>
-          )
-      }}</ThemeContext.Consumer>
+      <AuthContext.Consumer>{(authContext) => (
+        <ThemeContext.Consumer>{(themeContext) => {
+          const { isLightTheme, light, dark } = themeContext;
+          const theme = isLightTheme ? light : dark;
+          const { isAuthenticated } = authContext;
+          console.log(isAuthenticated);
+            return(
+              <nav style={{background: theme.ui, color: theme.text}}>
+                  <h1>Context App</h1>
+                  <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                  </ul>
+                </nav>
+            )
+        }}</ThemeContext.Consumer>
+      )}</AuthContext.Consumer>
     );
   }
 }
